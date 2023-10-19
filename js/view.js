6104a230e797d89d8c44c06d7d5faed5d17548d0
a-contrast <a.contrast@yandex.ru>
2023-10-19 14:54:47 +0300

    Исправления карточек на главной и каталога, мелкие правки
    Переделаны карточки на главной без выравнивания текстового содержимого по вертикали, переделаны карточки в каталоге без area, в модалке содержимое дополнительного текста к полям из after перенес в разметку, убран JS из разметки, между чекбоксами/радиокнопками и подписью пространство теперь тоже кликабельно, социконки увеличены в размере вместо gap, убран лишний код

--- js/view.js
+++ js/view.js
@@ -1,23 +1,18 @@
 let viewLinks = document.querySelectorAll('.view-link');
 let productList = document.querySelector('.product-list');
 
-for (let viewLink of viewLinks) {
-  viewLink.onclick = function(event) {
-    event.preventDefault();
+if (viewLinks && productList) {
+  viewLinks.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+      evt.preventDefault();
 
-    viewLinks.forEach(function(element) {
-      element.classList.remove('view-link-active');
+      viewLinks.forEach((nonActiveLink) => {
+        nonActiveLink.classList.remove('view-link-active');
+      });
+      link.classList.add('view-link-active');
+
+      let linkData = link.dataset.view;
+      productList.dataset.view = linkData;
     });
-    viewLink.classList.add('view-link-active');
-    if (viewLink.classList.contains('view-link-preview')) {
-      productList.classList.add('product-list-preview');
-      productList.classList.remove('product-list-table');
-    } else if (viewLink.classList.contains('view-link-table')) {
-      productList.classList.add('product-list-table');
-      productList.classList.remove('product-list-preview');
-    } else {
-      productList.classList.remove('product-list-preview');
-      productList.classList.remove('product-list-table');
-    }
-  }
-};
+  });
+}
